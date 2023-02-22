34da42319cea05e82320a70ccb98dec7b4d876c2
n1colashka <deckards2505@gmail.com>
2023-02-22 13:45:19 +0600

    add responsive

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,5 +1,5 @@
 import Swiper, { Pagination, Navigation } from 'swiper';
-
+import SimpleBar from 'simplebar';
 Swiper.use([Pagination, Navigation]);
 
 function initMobileSlider(parent, wrapper, items, breakpoint = 768) {
@@ -34,44 +34,71 @@ function initMobileSlider(parent, wrapper, items, breakpoint = 768) {
 
 // initMobileSlider(".comfort__slider", ".comfort__list", ".comfort__item");
 
-const whyPaginationItems = document.querySelectorAll('.why-pagination__item');
-const whyPaginationPanel = document.querySelector('.why-pagination__panel');
+function initWhySlider() {
+  const pagination = document.querySelector('.why-pagination');
+  const paginationItems = document.querySelectorAll('.why-pagination__item');
+  const paginationPanel = document.querySelector('.why-pagination__panel');
 
-const whySwiper = new Swiper('.why__slider', {
-  spaceBetween: 50,
-  navigation: {
-    prevEl: '.why__prev',
-    nextEl: '.why__next',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  on: {
-    slideChange(swiper) {
+  const whySwiper = new Swiper('.why__slider', {
+    spaceBetween: 50,
+    navigation: {
+      prevEl: '.why__prev',
+      nextEl: '.why__next',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    on: {
+      slideChange(swiper) {
+        const prevActiveItem = document.querySelector(
+          '.why-pagination__item.is-active'
+        );
+        const newActiveItem = paginationItems[swiper.activeIndex];
+        const leftPos =  newActiveItem.getBoundingClientRect().left - pagination.offsetLeft;
+
+        prevActiveItem.classList.remove('is-active');
+        newActiveItem.classList.add('is-active');
+
+        paginationPanel.style.transform = `translateX(${leftPos}px)`;
+      },
+    },
+  });
+
+  paginationItems.forEach((el, index) => {
+    el.addEventListener('click', () => {
+      if (el.classList.contains('is-active')) {
+        return;
+      }
       const prevActiveItem = document.querySelector(
         '.why-pagination__item.is-active'
       );
-      const newActiveItem = whyPaginationItems[swiper.activeIndex];
-
       prevActiveItem.classList.remove('is-active');
-      newActiveItem.classList.add('is-active');
-      whyPaginationPanel.style.transform = `translateX(${newActiveItem.offsetLeft}px)`;
-    },
-  },
-});
 
-whyPaginationItems.forEach((el, index) => {
-  el.addEventListener('click', () => {
-    if (el.classList.contains('is-active')) {
-      return;
-    }
-    const prevActiveItem = document.querySelector(
+      el.classList.add('is-active');
+      whySwiper.slideTo(index);
+    });
+  });
+
+  const paginationScroll = document.querySelector('.why-pagination__scroll');
+
+  const simpleBar = new SimpleBar(paginationScroll);
+  simpleBar.getScrollElement().addEventListener('scroll', function(e) {
+    const activeItem = document.querySelector(
       '.why-pagination__item.is-active'
     );
-    prevActiveItem.classList.remove('is-active');
+    const leftPos = activeItem.getBoundingClientRect().left - pagination.offsetLeft;
 
-    el.classList.add('is-active');
-    whySwiper.slideTo(index);
+    paginationPanel.style.transform = `translateX(${leftPos}px)`;
+    paginationPanel.style.transition = `none`;
+    console.log(leftPos)
+
+    setTimeout(() => {
+      paginationPanel.style.transition = ``;
+    }, 50)
   });
-});
+
+}
+
+
+initWhySlider();
